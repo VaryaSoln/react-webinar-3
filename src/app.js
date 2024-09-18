f6d2ade2fbd684d06c48ff60b1e2eeb62091d9de
Varvara Solnyshkina <SoVa10_04@mail.ru>
2024-09-18 22:23:57 +0300

    homework 2 part 1

--- src/app.js
+++ src/app.js
@@ -13,33 +13,22 @@ function App({ store }) {
   const list = store.getState().list;
 
   const callbacks = {
-    onDeleteItem: useCallback(
-      code => {
-        store.deleteItem(code);
-      },
-      [store],
-    ),
 
-    onSelectItem: useCallback(
+   /*  onSelectItem: useCallback(
       code => {
         store.selectItem(code);
       },
       [store],
-    ),
-
-    onAddItem: useCallback(() => {
-      store.addItem();
-    }, [store]),
+    ), */
   };
 
   return (
     <PageLayout>
       <Head title="Приложение на чистом JS" />
-      <Controls onAdd={callbacks.onAddItem} />
+      <Controls />
       <List
         list={list}
-        onDeleteItem={callbacks.onDeleteItem}
-        onSelectItem={callbacks.onSelectItem}
+        
       />
     </PageLayout>
   );
--- src/components/item/index.js
+++ src/components/item/index.js
@@ -4,40 +4,29 @@ import { plural } from '../../utils';
 import './style.css';
 
 function Item(props) {
-  // Счётчик выделений
-  const [count, setCount] = useState(0);
 
   const callbacks = {
     onClick: () => {
-      props.onSelect(props.item.code);
-      if (!props.item.selected) {
-        setCount(count + 1);
-      }
-    },
-    onDelete: e => {
-      e.stopPropagation();
-      props.onDelete(props.item.code);
+    
     },
   };
 
   return (
     <div
-      className={'Item' + (props.item.selected ? ' Item_selected' : '')}
+      className={'Item'}
       onClick={callbacks.onClick}
     >
       <div className="Item-code">{props.item.code}</div>
       <div className="Item-title">
         {props.item.title}{' '}
-        {count
-          ? ` | Выделяли ${count} ${plural(count, {
-              one: 'раз',
-              few: 'раза',
-              many: 'раз',
-            })}`
-          : ''}
+        
+      </div>
+      <div className="Item-price">
+        {`${props.item.price} ₽` }
+
       </div>
       <div className="Item-actions">
-        <button onClick={callbacks.onDelete}>Удалить</button>
+        <button onClick={()=>{}}>Добавить</button>
       </div>
     </div>
   );
